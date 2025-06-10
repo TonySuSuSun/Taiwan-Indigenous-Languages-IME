fc01e814dc84d8e2466e9fb704aba19d827c9850
TonySuSuSun <charactor38.tony@gmail.com>
2025-06-10 19:26:56 +0800

    Initial Commit

---
+++ Taiwan Indigenous Languages IME/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taiwan Indigenous Languages IME
+//
+//  Created by 蘇炫瑋 on 2025/6/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Taiwan Indigenous Languages IMETests/Taiwan_Indigenous_Languages_IMETests.swift
@@ -0,0 +1,17 @@
+//
+//  Taiwan_Indigenous_Languages_IMETests.swift
+//  Taiwan Indigenous Languages IMETests
+//
+//  Created by 蘇炫瑋 on 2025/6/10.
+//
+
+import Testing
+@testable import Taiwan_Indigenous_Languages_IME
+
+struct Taiwan_Indigenous_Languages_IMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
